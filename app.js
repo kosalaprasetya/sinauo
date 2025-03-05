0bd99587aa52a22c1e4265708e155db0bd44ba78
BrandonRiconga <bricongaw@gmail.com>
2025-03-05 15:29:06 +0700

    Add login.ejs, change router

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const port = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.set(`view engine`, 'ejs');
 app.use(router);
+app.use(express.static('assets'));
 
 app.listen(port, () => console.log(`app up and running on port ${port}`));
--- routers/routes.js
+++ routers/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Controller = require('../controllers/controllers.js');
 
-router.get('/', (req, res) => res.send(`okdgadfsfdsf43656ga`));
+router.get('/', (req, res) => res.render('login.ejs'));
 
 module.exports = router;
