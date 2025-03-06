701abcf97db8cf16179b50683684af91f7c07224
kosalaprasetya <kosalaprasetya@gmail.com>
2025-03-06 18:58:20 +0700

    fix(controller):fix some errors

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,31 +1,27 @@
-
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const { User, UserEnrollment, Course, CourseCategory, Category } = require('../models/index.js');
-const session = require('express-session')
-const {Op, where} = require('sequelize')
+const session = require('express-session');
+const { Op, where } = require('sequelize');
 
 class Controller {
+  static async showHome(req, res) {
+    try {
+      let role = req.session.userRole;
 
-    static async showHome(req,res){
-        try{
-            let role = req.session.userRole
-
-            res.render('home', {role})
-        }
-        catch (error){
-            res.send(error)
-        }
+      res.render('home', { role });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async showRegister(req,res){
-        try{
-            let {errors, path} = req.query
-            res.render('register', {errors, path})
-        }
-        catch (error){
-            res.send(error)
-        }
+  static async showRegister(req, res) {
+    try {
+      let { errors, path } = req.query;
+      res.render('register', { errors, path });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
   static async postRegister(req, res) {
     try {
@@ -69,26 +65,17 @@ class Controller {
 
         if (isValidPw) {
           return res.redirect('/home');
-        } else if(isValidPw){
-          req.session.userId = data.id
-          req.session.userRole = data.role
+        } else if (isValidPw) {
+          req.session.userId = data.id;
+          req.session.userRole = data.role;
 
-          return res.redirect('/home')
-        }
-        else{
-        const error = "Invalid Email / Password" 
-        return res.redirect(`/?errors=${error}`)
-             }
-        } else{
-        req.session.user={}
-        const error = "Invalid Email / Password" 
-        return res.redirect(`/?errors=${error}`)
-        }   
-        }
-        catch(error){
-            res.send(error)
+          return res.redirect('/home');
+        } else {
+          const error = 'Invalid Email / Password';
+          return res.redirect(`/?errors=${error}`);
         }
       } else {
+        req.session.user = {};
         const error = 'Invalid Email / Password';
         return res.redirect(`/?errors=${error}`);
       }
@@ -115,131 +102,121 @@ class Controller {
       res.send(error);
       console.log(error);
     }
-
-    static async getLogout(req,res){
-        try{
-            req.session.destroy((err)=> {
-                if(err){
-                    res.send(err)
-                }
-                else{
-                    res.redirect('/')
-                }
-            })
+  }
+  static async getLogout(req, res) {
+    try {
+      req.session.destroy((err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.redirect('/');
         }
-        catch (error){
-            res.send(error)
-        }
+      });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async studentList(req,res){
-        try{
-            let {name} = req.query
-            let role = req.session.userRole
-            let data = await User.findAll({
-                where: {
-                    role: {
-                        [Op.ne] : 'admin'
-                    }
-                },
-                order: [['id', 'ASC']]
-            })
-            data = data.map(el=> el.dataValues)
-            res.render('manageStudent', {role, data, name})
-        }
-        catch (error){
-            res.send(error)
-        }
+  static async studentList(req, res) {
+    try {
+      let { name } = req.query;
+      let role = req.session.userRole;
+      let data = await User.findAll({
+        where: {
+          role: {
+            [Op.ne]: 'admin',
+          },
+        },
+        order: [['id', 'ASC']],
+      });
+      data = data.map((el) => el.dataValues);
+      res.render('manageStudent', { role, data, name });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async addStudent(req,res){
-        try{
-            let{errors, path} = req.query
-            let role = req.session.userRole
-            res.render('addStudents', {role, errors, path})
-        }
-        catch(error){
-            res.send(error)
-        }
+  static async addStudent(req, res) {
+    try {
+      let { errors, path } = req.query;
+      let role = req.session.userRole;
+      res.render('addStudents', { role, errors, path });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async postStudent(req,res){
-        try{
-            await User.create(req.body)
+  static async postStudent(req, res) {
+    try {
+      await User.create(req.body);
 
-            res.redirect('/home/manage/')
-        }
-        catch(error){
-            if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
-                let path = error.errors.map(el=> el.path)
-                let message = error.errors.map(el=> el.message)
+      res.redirect('/home/manage/');
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        let path = error.errors.map((el) => el.path);
+        let message = error.errors.map((el) => el.message);
 
-                res.redirect(`/home/manage/addStudent?errors=${message}&path=${path}`)
-            }
-            else{
-                console.log(error)
-                res.send(error)
-            }
-        }
+        res.redirect(`/home/manage/addStudent?errors=${message}&path=${path}`);
+      } else {
+        console.log(error);
+        res.send(error);
+      }
     }
+  }
 
-    static async editStudent(req,res){
-        try{
-            let{errors, path} = req.query
-            let {id} = req.params
-            let role = req.session.userRole
-            let student = await User.findByPk(+id)
+  static async editStudent(req, res) {
+    try {
+      let { errors, path } = req.query;
+      let { id } = req.params;
+      let role = req.session.userRole;
+      let student = await User.findByPk(+id);
 
-            res.render('editStudent', {student, role, errors, path})
-        }
-        catch(error){
-            res.send(error)
-        }
+      res.render('editStudent', { student, role, errors, path });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async postEditStudent(req,res){
-        try{
-            let{id} = req.params
-            await User.update(req.body, {
-                where: {
-                    id: +id
-                }
-            })
-            res.redirect('/home/manage')
-        }
-        catch(error){
-            let {id} = req.params
-            if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
-                let path = error.errors.map(el=> el.path)
-                let message = error.errors.map(el=> el.message)
+  static async postEditStudent(req, res) {
+    try {
+      let { id } = req.params;
+      await User.update(req.body, {
+        where: {
+          id: +id,
+        },
+      });
+      res.redirect('/home/manage');
+    } catch (error) {
+      let { id } = req.params;
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        let path = error.errors.map((el) => el.path);
+        let message = error.errors.map((el) => el.message);
 
-                res.redirect(`/home/manage/edit/${id}?errors=${message}&path=${path}`)
-            }
-            else{
-                res.send(error)
-            }
-        }
+        res.redirect(`/home/manage/edit/${id}?errors=${message}&path=${path}`);
+      } else {
+        res.send(error);
+      }
     }
+  }
 
-    static async deleteUser(req,res){
-        try{
-            let {id} = req.params
-            let role = req.session.userRole
+  static async deleteUser(req, res) {
+    try {
+      let { id } = req.params;
+      let role = req.session.userRole;
 
-            let user = await User.findByPk(+id)
-            let name = user.name
-            await User.destroy({
-                where : {
-                    id: +id
-                }
-            })
+      let user = await User.findByPk(+id);
+      let name = user.name;
+      await User.destroy({
+        where: {
+          id: +id,
+        },
+      });
 
-            res.redirect(`/home/manage?name=${name}`)
-        }
-        catch (error){
-            res.send(error)
-        }
+      res.redirect(`/home/manage?name=${name}`);
+    } catch (error) {
+      res.send(error);
     }
+  }
 }
 
 module.exports = Controller;
